3ed05436f44ba366a9a2cda4fb33add2540ded52
Ian Sergio Helfenberger <iansergiohelf@gmail.com>
2025-08-03 00:14:22 -0300

    todo crud implementation

---
+++ src/main/java/api/todo/controllers/TodoController.java
@@ -0,0 +1,48 @@
+package api.todo.controllers;
+
+import api.todo.entities.Todo;
+import api.todo.services.TodoService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+
+    private final TodoService todoService;
+
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Todo> insertTodo(@RequestBody Todo todo) {
+        Todo savedTodo = todoService.insertTodo(todo);
+        URI location = URI.create("/todos/" + savedTodo.getId());
+        return ResponseEntity.created(location).body(savedTodo);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Todo>> getAllTodos() {
+        return ResponseEntity.ok(todoService.getAllTodos());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Todo> getTodoById(@PathVariable Long id) {
+        return ResponseEntity.ok(todoService.getTodoById(id));
+    }
+
+    @PutMapping
+    public ResponseEntity<Todo> updateTodo(@RequestBody Todo todo) {
+        return ResponseEntity.ok(todoService.updateTodo(todo));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTodo(@PathVariable Long id) {
+        todoService.deleteTodo(id);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ src/main/java/api/todo/services/TodoService.java
@@ -0,0 +1,49 @@
+package api.todo.services;
+
+import api.todo.entities.Todo;
+import api.todo.repositories.TodoRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@Service
+public class TodoService {
+
+    private final TodoRepository todoRepository;
+
+    public TodoService(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+    public Todo insertTodo(Todo todo) {
+        if (todo.getId() != null && todoRepository.existsById(todo.getId())) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "Todo already exists");
+        }
+        return todoRepository.save(todo);
+    }
+
+    public List<Todo> getAllTodos() {
+        return todoRepository.findAll();
+    }
+
+    public Todo getTodoById(Long id) {
+        return todoRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Todo not found with id " + id));
+    }
+
+    public Todo updateTodo(Todo todo) {
+        if (todo.getId() == null || !todoRepository.existsById(todo.getId())) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Todo can not be updated, id does not exist.");
+        }
+        return todoRepository.save(todo) ;
+    }
+
+    public void deleteTodo(Long id) {
+        if (todoRepository.findById(id).isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Todo not found with id " + id);
+        }
+        todoRepository.deleteById(id);
+    }
+}
